1764aa83324cb9836e695579a6241ba9204c9b7c
Nicholas2208 <nikolaj.belyakov.84@gmail>
2024-01-27 22:24:08 +0300

    Validate input data

--- accounts/src/main/java/com/nwhite/accounts/dto/AccountDto.java
+++ accounts/src/main/java/com/nwhite/accounts/dto/AccountDto.java
@@ -1,13 +1,19 @@
 package com.nwhite.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountDto {
 
+    @NotEmpty(message = "AccountNumber must not be null or empty")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "AccountType must not be null or empty")
     private String accountType;
 
+    @NotEmpty(message = "BranchAddress must not be null or empty")
     private String branchAddress;
 }
